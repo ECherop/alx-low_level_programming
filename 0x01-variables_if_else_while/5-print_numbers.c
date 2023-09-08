3971f347b958c4e871edca26c34a58552245e76d
edith <edithbett4@gmail.com>
2023-09-08 09:24:19 -0700

    zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Check alphabet'
+* Return: Always 0 when successful
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
