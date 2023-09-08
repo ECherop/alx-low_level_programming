9a225954143e489d198743d9f997ca2fe61dc9c2
edith <edithbett4@gmail.com>
2023-09-08 10:01:13 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Check alphabet'
+* Return: Always 0 when successful
+*/
+int main(void)
+{
+int n = 122;
+while (in >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
