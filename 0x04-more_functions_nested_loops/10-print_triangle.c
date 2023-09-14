be8cd6fbae4c0acb13db43c524e239a4a4ccc037
edith <edithbett4@gmail.com>
2023-09-14 09:26:50 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_triangle - function that prints
+* diagonals
+* @size: the size of the triangle
+* Return: 1 if c is a digit. 0 otherwise.
+*/
+void print_triangle(int size)
+{
+int row, hashes, spaces;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (spaces = size - row; spaces >= 1; spaces--)
+{
+_putchar(' ');
+}
+for (hashes = 1; hashes <= row; hashes++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
